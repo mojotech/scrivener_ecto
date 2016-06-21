a5c29434b9ac768a3de502f56de618b9ecf812cb
drew <drew@drewolson.org>
2016-06-21 08:48:52 -0500

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scrivener.Ecto.Mixfile do
   def project do
     [
       app: :scrivener_ecto,
-      version: "1.0.0-dev",
+      version: "1.0.0",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env),
       package: package,
@@ -28,8 +28,8 @@ defmodule Scrivener.Ecto.Mixfile do
 
   defp deps do
     [
-      {:scrivener, git: "https://github.com/drewolson/scrivener", branch: "v2"},
-      {:ecto, "~> 2.0.0-rc"},
+      {:scrivener, "~> 2.0"},
+      {:ecto, "~> 2.0"},
       {:dialyxir, "~> 0.3.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.11.0", only: :dev},
