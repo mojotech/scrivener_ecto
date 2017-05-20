c54333599d34b2b3d5f3e88da0a64ed22a8dcdb3
Ege Ersoz <ege.ersoz@gmail.com>
2017-05-19 23:28:49 -0500

    implement pagination with last_seen_id instead of offset

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,8 @@ config :scrivener_ecto, Scrivener.Ecto.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   database: "scrivener_test",
-  username: System.get_env("SCRIVENER_ECTO_DB_USER") || System.get_env("USER")
+  username: System.get_env("PG_DB_USER"),
+  password: System.get_env("PG_DB_PASSWORD")
 
 config :logger, :console,
   level: :error
--- lib/scrivener/paginater/ecto/query.ex
+++ lib/scrivener/paginater/ecto/query.ex
@@ -6,8 +6,14 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
   @moduledoc false
 
   @spec paginate(Ecto.Query.t, Scrivener.Config.t) :: Scrivener.Page.t
-  def paginate(query, %Config{page_size: page_size, page_number: page_number, module: repo, caller: caller}) do
+  def paginate(query, %Config{page_size: page_size, page_number: page_number, last_seen_id: last_seen_id, module: repo, caller: caller}) do
     total_entries = total_entries(query, repo, caller)
+    entries =
+      if is_nil(last_seen_id) == false do
+        entries_no_offset(query, repo, last_seen_id, page_size, caller)
+      else
+        entries(query, repo, page_number, page_size, caller)
+      end
 
     %Page{
       page_size: page_size,
@@ -24,6 +30,13 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
     query
     |> limit(^page_size)
     |> offset(^offset)
+    |> repo.all(caller: caller)
+  end
+
+  defp entries_no_offset(query, repo, last_seen_id, page_size, caller) do
+    query
+    |> limit(^page_size)
+    |> where([p], p.id > ^last_seen_id)
     |> repo.all(caller: caller)
   end
 
