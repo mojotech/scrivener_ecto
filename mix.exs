933ea8fe5edbf1b67545de33580048f0c5d77628
drew <drew@drewolson.org>
2017-10-27 11:24:19 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Scrivener.Ecto.Mixfile do
       {:ecto, "~> 2.0"},
       {:dialyxir, "~> 0.5.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:ex_doc, "~> 0.16.0", only: :dev},
+      {:ex_doc, "~> 0.18.0", only: :dev},
       {:postgrex, "~> 0.11.0 or ~> 0.12.0 or ~> 0.13.0", optional: true}
     ]
   end
