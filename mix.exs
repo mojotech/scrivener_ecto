672091b466d5896ee960afc7f96eeab804e1bb05
Maarten Jacobs <m.jacobs@defacto.nl>
2018-09-13 14:00:48 +0200

    upgrade to ecto 3.x

--- mix.exs
+++ mix.exs
@@ -42,11 +42,11 @@ defmodule Scrivener.Ecto.Mixfile do
   defp deps do
     [
       {:scrivener, "~> 2.4"},
-      {:ecto, "~> 2.0"},
+      {:ecto, git: "https://github.com/elixir-ecto/ecto.git"},
       {:dialyxir, "~> 0.5.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.18.0", only: :dev},
-      {:postgrex, "~> 0.11.0 or ~> 0.12.0 or ~> 0.13.0", optional: true}
+      {:postgrex, "~> 0.14.0-dev", override: true, github: "elixir-ecto/postgrex"}
     ]
   end
 
