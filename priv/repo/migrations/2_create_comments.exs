a2567dbf7faa4d23bbdd2ffd63be064f30a6a8f8
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-08-23 21:43:21 +0200

    Remove prefix from comments and key_values migrations

--- priv/repo/migrations/2_create_comments.exs
+++ priv/repo/migrations/2_create_comments.exs
@@ -2,7 +2,7 @@ defmodule Scrivener.Repo.Migrations.CreateComments do
   use Ecto.Migration
 
   def change do
-    create table(:comments, prefix: prefix) do
+    create table(:comments) do
       add :body, :string
       add :post_id, :integer
 
--- priv/repo/migrations/3_create_key_values.exs
+++ priv/repo/migrations/3_create_key_values.exs
@@ -2,7 +2,7 @@ defmodule TestRepo.Migrations.CreateKeyValues do
   use Ecto.Migration
 
   def change do
-    create table(:key_values, primary_key: false, prefix: prefix) do
+    create table(:key_values, primary_key: false) do
       add :key, :string, primary_key: true
       add :value, :string
     end
